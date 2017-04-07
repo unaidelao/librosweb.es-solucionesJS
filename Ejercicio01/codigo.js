e6db002cb446e6d032737ed4cbe59cb9bbf1b3dc
unaidelao <unai.delao@gmail.com>
2017-04-08 00:19:02 +0200

    Add cogido.js-Ejercicio01

---
+++ Ejercicio01/codigo.js
@@ -0,0 +1,19 @@
+/*
+ * Ejercicio 1
+ *
+ * Modificar el primer script para que:
+ * (1) Todo el código JavaScript se encuentre en un archivo externo llamado
+ * código.js y el script siga funcionando de la misma manera.
+ *
+ * (2) Después del primer mensaje, se debe mostrar otro mensaje que diga
+ * "Soy el primer script".
+ *
+ * (3) Añadir algunos comentarios que expliquen el funcionamiento del código.
+ *
+ */
+
+// primer mensaje de alerta
+alert("Hola Mundo!");
+
+// segundo mensaje de alerta
+alert("Soy el primer script");
