3e70eddc5abb192a1f54e91166ad7b7dd3ac0362
unaidelao <unai.delao@gmail.com>
2017-04-10 16:36:09 +0200

    Add codigo.js - Ejercicio02

---
+++ Ejercicio02/codigo.js
@@ -0,0 +1,15 @@
+/*
+ * Ejercicio 02
+ *
+ * Modificar el primer script para que:
+ * (1) El mensaje que se muestra al usuario se almacene en una variable
+ * llamada mensaje y el funcionamiento del script sea el mismo.
+ *
+ * (2) El mensaje mostrado sea como el de la imagen.
+ */
+
+// variable que almacena el mensaje
+var mensaje = 'Hola Mundo!\nQue facil es incluir \'comillas simples\'\ny \"comillas dobles\"';
+
+// mensaje de alerta
+alert(mensaje);
