c40759248f0a9f5eba45417a52d9e4909dea17f9
unaidelao <unai.delao@gmail.com>
2017-04-12 17:32:33 +0200

    Add codigo.js - Ejercicio03

---
+++ Ejercicio03/codigo.js
@@ -0,0 +1,13 @@
+/*
+ * Ejercicio 03
+ *
+ * Crear un array llamado meses y que almacene el nombre de los doce meses del
+ * año. Mostrar por pantalla los doce nombres utilizando la función alert().
+ */
+
+var meses = [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+ "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+for (var i = 0; i < meses.length; i++) {
+  alert(meses[i]);
+}
