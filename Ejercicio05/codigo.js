a6732129398ea94736688d7b9b5246101aafd53f
unaidelao <unai.delao@gmail.com>
2017-04-15 13:34:25 +0200

    Add codigo.js - Ejercicio 05

---
+++ Ejercicio05/codigo.js
@@ -0,0 +1,22 @@
+/*
+ * Ejercicio 05
+ *
+ * Completar las condiciones de los if del siguiente script para que los
+ * mensajes de los alert() se muestren siempre de forma correcta:
+ */
+
+ var numero1 = 5;
+ var numero2 = 8;
+
+ if(numero1 <= numero2) {
+   alert("numero1 no es mayor que numero2");
+ }
+ if(numero2 > 0) {
+   alert("numero2 es positivo");
+ }
+ if(numero1 < 0 || numero1 != 0) {
+   alert("numero1 es negativo o distinto de cero");
+ }
+ if(++numero1 < numero2) {
+   alert("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+ }
